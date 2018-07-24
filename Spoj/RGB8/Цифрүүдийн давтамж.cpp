df8e2d95ec6d3709b65cc57479f2d00e679510c1
Tseku <41584870+skywithlight@users.noreply.github.com>
2018-07-24 16:34:53 +0800

    Create Цифрүүдийн давтамж.cpp

---
+++ Spoj/RGB8/Цифрүүдийн давтамж.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+ 
+    int  n, count = 0, number, j, i, s = 0;
+    int a[101];
+    cin >> number;
+    a[count] = number;
+    count ++;
+ 
+    do {
+        cin >> number;
+        a[count] = number;
+        count ++;
+    } while (number != 0);
+ 
+    n = count - 1;
+ 
+ 
+    j = 1;
+    while (j < 10){
+        s = 0;
+        i = 0;
+        while (i < n){
+            if (j == a[i]){
+                s = s + 1;
+            }
+            i++;
+        }
+        cout << s << endl;        
+        j++;
+    }
+    
+    return 0;
+} 
