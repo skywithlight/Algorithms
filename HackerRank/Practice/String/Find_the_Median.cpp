fe2b9729288ab88d1417a7328df7b9b5e443c957
Tseku <41584870+skywithlight@users.noreply.github.com>
2018-07-24 11:49:43 +0800

    Create Find_the_Median.cpp

---
+++ HackerRank/Practice/String/Find_the_Median.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findMedian(vector <int> arr) {
+    sort (arr.begin(), arr.end());
+    int tem = arr[arr.size() / 2];
+    return tem;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0; arr_i < n; arr_i++){
+    	cin >> arr[arr_i];
+    }
+    int result = findMedian(arr);
+    cout << result << endl;
+    return 0;
+}
