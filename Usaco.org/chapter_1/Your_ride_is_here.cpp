0f2bdd1564d58fa2882f20210beb4afc86025641
Tseku <41584870+skywithlight@users.noreply.github.com>
2018-10-14 17:11:19 +0800

    Update Your_ride_is_here.cpp
    problem link: http://train.usaco.org/usacoprob2?a=QN3N1SxXWvb&S=ride

--- Usaco.org/chapter_1/Your_ride_is_here.cpp
+++ Usaco.org/chapter_1/Your_ride_is_here.cpp
@@ -1,1 +1,36 @@
+/*
+ID: ntselme1
+TASK: ride
+LANG: C++
+*/
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+    freopen ("ride.in", "r", stdin);
+    freopen ("ride.out", "w", stdout);
+
+    string n, m;
+    cin >> n >> m;
+
+    int pro1 = 1, pro2 = 1;
+
+    for (int i = 0; i < n.size(); i++){
+        int tem = n[i];
+        pro1 *= (tem - 64);
+    }
+
+    for (int i = 0; i < m.size(); i++){
+        int tem = m[i];
+        pro2 *= (tem - 64);
+    }
+
+    if (pro1 % 47 == pro2 % 47)
+        cout << "GO" << endl;
+    else 
+        cout << "STAY" << endl;
+
+    return 0;
+}
